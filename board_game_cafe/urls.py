7dd2ead1dfd22a0e5403d7a51f331af106a3964a
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-11-24 00:20:10 +0700

    feat: Initialize HomeView (After config many thing It can display the index.html)

--- board_game_cafe/urls.py
+++ board_game_cafe/urls.py
@@ -1,7 +1,9 @@
+"""Urls module for travel to each poll html."""
 from django.urls import path
 
 from . import views
 
+app_name = "board_game_cafe"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.HomeView.as_view(), name="index"),
 ]
