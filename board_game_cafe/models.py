5c34cf6bf1cb348a21207e1df2ae6967536dc612
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-25 15:12:28 +0700

    feat: added the rest of the models

--- board_game_cafe/models.py
+++ board_game_cafe/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Customer(models.Model):
@@ -6,12 +7,20 @@ class Customer(models.Model):
     customer_name = models.CharField(max_length=30, default=None)
     contact = models.CharField(max_length=30, default=None)
 
+    class Meta:
+        app_label = 'board_game_cafe'
+        db_table = 'Customer'
+
 
 class Booking(models.Model):
     booking_id = models.AutoField(primary_key=True, unique=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     item_type = models.CharField(max_length=30, default=None)
     item_id = models.CharField(max_length=30, default=None)
+    
+    class Meta:
+        app_label = 'board_game_cafe'
+        db_table = 'Booking'
 
 
 class Rental(models.Model):
@@ -19,7 +28,31 @@ class Rental(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     item_type = models.CharField(max_length=30, default=None)
     item_id = models.CharField(max_length=30, default=None)
-    rent_date = models.DateTimeField(auto_now_add=True)
-    due_date = models.DateTimeField()
+    rent_date = models.DateTimeField(default=timezone.now)
+    due_date = models.DateTimeField(default=timezone.now+timezone.timedelta(days=3))
     status = models.CharField(max_length=30, default=None)
 
+    @property
+    def duration(self):
+        return timezone.now() - self.rent_date
+
+    class Meta:
+        app_label = 'board_game_cafe'
+        db_table = 'Rental'
+
+
+class Table(models.Model):
+    table_id = models.AutoField(primary_key=True, unique=True)
+    capacity = models.IntegerField(default=4)
+
+
+class BoardGameGroup(models.Model):
+    group_name = models.Charfield(max_length=30, default="small")
+    base_fee = models.IntegerField(default=5)
+
+
+class BoardGame(models.Model):
+    boardgame_id = models.AutoField(primary_key=True, unique=True)
+    boardgame_name = models.CharField(max_length=30, default=None)
+    category_id = models
+    group = models.ForeignKey(BoardGameGroup)
