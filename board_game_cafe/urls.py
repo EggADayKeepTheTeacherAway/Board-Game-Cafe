816c90b97055d5370262e3b638dfd290d7c62956
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-27 02:55:48 +0700

    fix: change path url

--- board_game_cafe/urls.py
+++ board_game_cafe/urls.py
@@ -7,11 +7,10 @@ from . import views
 app_name = "board_game_cafe"
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="book", permanent=False), name="index"),
+    path("", RedirectView.as_view(pattern_name="board_game_cafe:book", permanent=False), name="index"),
     path("book/", views.HomeView.as_view(), name="book"),
     path("rent/", views.RentView.as_view(), name="rent"),
     path("return/", views.ReturnView.as_view(), name="return"),
-    path("input/", views.PostView.as_view(), name="post_view"),
     path("stat/", views.StatView.as_view(), name="stat"),
     path("profile/", views.ProfileView.as_view(), name="profile"),
 ]
