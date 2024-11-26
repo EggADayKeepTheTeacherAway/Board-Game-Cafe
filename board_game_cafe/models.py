eba94408b6b327fb1cf53c7da19fa97742ab7191
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-27 06:30:02 +0700

    feat: add a lot of function and property to Rental

--- board_game_cafe/models.py
+++ board_game_cafe/models.py
@@ -24,6 +24,7 @@ class Booking(models.Model):
         app_label = 'board_game_cafe'
         db_table = 'Booking'
 
+
 def next_three_days():
     return timezone.now() + timezone.timedelta(days=3)
 
@@ -38,6 +39,31 @@ class Rental(models.Model):
     return_date = models.DateTimeField(null=True)
     fee = models.IntegerField(null=True)
 
+    @property
+    def duration(self) -> int:
+        return (timezone.now() - self.rent_date).days
+
+    def is_overdue(self):
+        return not self.return_date or self.return_date > self.due_date
+
+    def compute_fee(self):
+        fee_handler = {"Table": 5,
+               "BoardGame": BoardGame.objects.get(
+                   boardgame_id=self.item_id).group.base_fee
+               }
+        fee = self.duration * fee_handler[self.item_type]
+        fee += max(0, fee_handler[self.item_type]*(self.duration-9)) # compute overdue fee
+        self.fee = fee
+        self.status = 'returned'
+        self.return_date = timezone.now()
+        self.save()
+
+    def get_item(self):
+        handle_item_type = {"Table": lambda id: Table.objects.get(table_id=id),
+                     "BoardGame": lambda id: BoardGame.objects.get(boardgame_id=id)}
+        
+        return handle_item_type[self.item_type](self.item_id)
+
     class Meta:
         app_label = 'board_game_cafe'
         db_table = 'Rental'
@@ -47,7 +73,6 @@ class Table(models.Model):
     table_id = models.AutoField(primary_key=True, unique=True)
     capacity = models.IntegerField(default=4)
 
-    @property
     def is_available(self):
         return self.table_id not in Rental.objects.filter(
             item_type='Table', status="rented").values_list('item_id', flat=True)
@@ -64,7 +89,6 @@ class BoardGameCategory(models.Model):
 class BoardGame(models.Model):
     boardgame_id = models.AutoField(primary_key=True, unique=True)
     boardgame_name = models.CharField(max_length=30, default=None)
-    category = models.CharField(max_length=30, default=None)
     group = models.ForeignKey(BoardGameGroup, on_delete=models.CASCADE)
     category = models.ForeignKey(BoardGameCategory, on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
