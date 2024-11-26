bc7d1128f2276994e5399342f6263d51c15f586a
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-27 01:12:09 +0700

    feat: add login and signup views

--- board_game_cafe/views.py
+++ board_game_cafe/views.py
@@ -2,16 +2,46 @@
 
 from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
+from django.contrib import messages
 from django.views import generic
-from models import Rental, Table, BoardGame
+from .forms import RegisterForm, LoginForm
+from .models import (Rental, Table, BoardGame,
+                     Customer, BoardGameCategory,
+                     BoardGameGroup)
 
 
-class LoginView(generic.ListView):
-    """Class for login page"""
-    template_name = "account.html"
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        customer_name = request.POST['customer_name']
+        password = request.POST['password']
+        user = Customer.objects.filter(customer_name=customer_name, password=password)
+        if user.exists():
+            user = user.get()
+            request.session['customer_id'] = user.customer_id 
+            return redirect('board_game_cafe:index')
+        messages.error(request,
+                       "You entered wrong username or password, or you forgot to sign up.")
+    return render(request, 'login.html', {'form': form})
 
-    def get_queryset(self):
-        return []
+
+def signup(request):
+    form = RegisterForm()
+    if request.method == "POST":
+        customer_name = request.POST['customer_name']
+        password = request.POST['password']
+        contact = request.POST['contact']
+        if Customer.objects.filter(customer_name=customer_name,
+                                       password=password,
+                                       contact=contact).exists():
+            messages.warning(request, "You already have an account.")
+            return redirect('login')
+        user = Customer.objects.create(customer_name=customer_name,
+                                    password=password,
+                                    contact=contact)
+        request.session['customer_id'] = user.customer_id
+        return redirect('board_game_cafe:index')
+    return render(request, 'signup.html', {'form': form})
 
 
 class HomeView(generic.ListView):
@@ -36,9 +66,10 @@ class RentView(generic.ListView):
     model = Rental
     template_name = "app/rent.html"
 
-    context_object_name = ''
+    context_object_name = 'item'
 
     def get_queryset(self):
+
         return []
 
 
