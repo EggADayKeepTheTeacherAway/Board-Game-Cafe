af6a5d156dca20921ac763c9f4fff244ea02413b
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-27 01:11:53 +0700

    feat: add default redirects

--- board_game_cafe/urls.py
+++ board_game_cafe/urls.py
@@ -5,10 +5,11 @@ from django.views.generic.base import RedirectView
 from . import views
 
 app_name = "board_game_cafe"
+
 urlpatterns = [
-    path("", views.LoginView.as_view(), name="login"),
-    path("book/<int:pk>", views.HomeView.as_view(), name="index"),
-    path("rent/<int:pk>", views.RentView.as_view(), name="rent"),
+    path("", RedirectView.as_view(pattern="book", permanent=False), name="index"),
+    path("book/", views.HomeView.as_view(), name="book"),
+    path("rent/", views.RentView.as_view(), name="rent"),
     path("return/", views.ReturnView.as_view(), name="return"),
     path("input/", views.PostView.as_view(), name="post_view"),
 ]
