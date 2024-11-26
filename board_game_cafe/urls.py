a628bad2232a0cfdf4f13d8fd4bf21981448f7d9
Rattanan RUNG-UTHAI <japanrisesgamer@gmail.com>
2024-11-26 18:10:58 +0700

    feat: added routing to urls

--- board_game_cafe/urls.py
+++ board_game_cafe/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = "board_game_cafe"
 urlpatterns = [
     path("", views.LoginView.as_view(), name="login"),
-    path("book/", views.HomeView.as_view(), name="index"),
-    path("rent/", views.RentView.as_view(), name="rent"),
+    path("book/<int:pk>", views.HomeView.as_view(), name="index"),
+    path("rent/<int:pk>", views.RentView.as_view(), name="rent"),
     path("return/", views.ReturnView.as_view(), name="return")
 ]
