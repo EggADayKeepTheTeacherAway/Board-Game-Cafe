f8ae790af2c470dbb223951ac845a2ee5ef0e174
Kasidet Uthaiwiwatkul <ichigamer111th@gmail.com>
2024-11-27 10:35:21 +0700

    style: class should have 2 line before it

--- board_game_cafe/models.py
+++ board_game_cafe/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
 class Customer(models.Model):
     customer_id = models.AutoField(primary_key=True, unique=True)
     customer_name = models.CharField(max_length=30, default=None)
@@ -27,6 +27,7 @@ class Booking(models.Model):
 
 def next_three_days():
     return timezone.now() + timezone.timedelta(days=3)
+
 
 class Rental(models.Model):
     rental_id = models.AutoField(primary_key=True, unique=True)
@@ -87,9 +88,11 @@ class BoardGameGroup(models.Model):
     group_name = models.CharField(max_length=30, default="small")
     base_fee = models.IntegerField(default=5)
 
+
 class BoardGameCategory(models.Model):
     category_id = models.AutoField(primary_key=True, unique=True)
     category_name = models.CharField(max_length=30, default="Dice")
+
 
 class BoardGame(models.Model):
     boardgame_id = models.AutoField(primary_key=True, unique=True)
